9cdffc95717848fa001c92a08aec01abd5ebdf89
Joe Pelz <joe.pelz@gmail.com>
2016-05-03 16:49:17 -0700

    Racoon character

--- Assets/Raccoon/racoon_readme.txt
+++ Assets/Raccoon/racoon_readme.txt
@@ -1,4 +1,5 @@
-Source: I.O.V
+Downloaded via Unity Asset Store
 
+Source: I.O.V
 
 https://www.assetstore.unity3d.com/en/#!/search/page=1/sortby=popularity/query=publisher:12836
--- Assets/scripts/movement.cs
+++ Assets/scripts/movement.cs
@@ -10,12 +10,32 @@ public class movement : MonoBehaviour {
     public Vector3 clickPos;
     public Vector2 clickVec;
     public double delta = 0.1;
+    private Animator m_Anim;            // Reference to the player's animator component.
 
-	// Use this for initialization
-	void Start () {
+    private void Awake() {
+        m_Anim = pal.GetComponent<Animator>();
+    }
+
+    // Use this for initialization
+    void Start () {
         clickPos = new Vector3(0, 0, 0);
     }
-	
+
+    void facingLeft(bool left) {
+        if (left) {
+            pal.transform.localScale = new Vector3(
+                -System.Math.Abs(pal.transform.localScale.x), 
+                pal.transform.localScale.y, 
+                pal.transform.localScale.z);
+        } else {
+            pal.transform.localScale = new Vector3(
+                System.Math.Abs(pal.transform.localScale.x), 
+                pal.transform.localScale.y, 
+                pal.transform.localScale.z);
+        }
+    }
+
+
 	// Update is called once per frame
 	void Update () {
         // detect left mouse click
@@ -31,9 +51,17 @@ public class movement : MonoBehaviour {
 
         // if the click position is more than delta away from the pal
         if ((Mathf.Abs(clickPos.y - pal.transform.position.y) > delta) || (Mathf.Abs(clickPos.x - pal.transform.position.x) > delta)) {
-                // set pal position to click position
-                pal.transform.Translate( Time.deltaTime * clickVec.x * 10, Time.deltaTime * clickVec.y * 10, 0);
+            if (clickVec.x < 0) {
+                facingLeft(true);
+            } else {
+                facingLeft(false);
             }
+            // set pal position to click position
+            pal.transform.Translate( Time.deltaTime * clickVec.x * 10, Time.deltaTime * clickVec.y * 10, 0);
+            m_Anim.SetFloat("vSpeed", clickVec.magnitude);
+        } else {
+            m_Anim.SetFloat("vSpeed", 0);
+        }
            
         // display the pal's position vector
         displayPos.text = pal.transform.position.x + ", " + pal.transform.position.y + ", " + pal.transform.position.z;
