3682aa9c8f85a8d1721f6977a52e49608f11bafe
Joe Pelz <joe.pelz@gmail.com>
2016-05-09 15:18:44 -0700

    added room controller for door, bed, wardrobe

---
+++ Assets/scripts/room_controller.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class room_controller : MonoBehaviour {
+    public GameObject wardrobeOpen;
+    public GameObject door;
+    public GameObject bed;
+
+    public void OpenDoor() {
+        Renderer r = door.GetComponent<Renderer>();
+        r.sortingOrder = -99;
+    }
+
+    public void CloseDoor() {
+        Renderer r = door.GetComponent<Renderer>();
+        r.sortingOrder = -101;
+    }
+
+    public void OpenWardrobe() {
+        Renderer r = wardrobeOpen.GetComponent<Renderer>();
+        r.sortingOrder = -99;
+    }
+
+    public void CloseWardrobe() {
+        Renderer r = wardrobeOpen.GetComponent<Renderer>();
+        r.sortingOrder = -101;
+    }
+
+    public void BedInFront() {
+        Renderer r = bed.GetComponent<Renderer>();
+        r.sortingOrder = 101;
+    }
+
+    public void BedBehind() {
+        Renderer r = bed.GetComponent<Renderer>();
+        r.sortingOrder = -101;
+    }
+}
