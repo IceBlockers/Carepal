73eafc11c0b6e57b79f2ce61b4314e79806e3a1e
Dylan Blake <dm_blake@live.com>
2016-05-10 14:28:04 -0700

    event manager testing

---
+++ Assets/scripts/EventManager.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using System.Collections;
+using System.Collections.Generic;
+
+public class EventManager : MonoBehaviour {
+
+	private Dictionary<string, UnityEvent> dictionary;
+	private static EventManager em;
+	
+	public static EventManager instance {
+		get {
+			if (!em) {
+				em = FindObjectOfType(typeof(EventManager)) as EventManager;
+				if (!em) {
+					Debug.Log("Could not find an EventManager object");
+				}
+				else {
+					em.Init();
+				}
+			}
+			return em;
+		}
+	}
+	
+	private void Init() {
+		if (dictionary == null) {
+			dictionary = new Dictionary<string, UnityEvent>();
+		}
+	}
+	
+	public static void StartListening(string eventName, UnityAction listener) {
+		UnityEvent thisEvent = null;
+		
+		if (instance.dictionary.TryGetValue(eventName, out thisEvent)) {
+			thisEvent.AddListener(listener);
+		} else {
+			thisEvent = new UnityEvent();
+			thisEvent.AddListener(listener);
+			instance.dictionary.Add(eventName, thisEvent);
+		}
+	}
+	
+	public static void StopListening(string eventName, UnityAction listener) {
+		if (instance.dictionary == null) {
+			return;
+		}
+		UnityEvent thisEvent = null;
+		if (instance.dictionary.TryGetValue(eventName, out thisEvent)) {
+			thisEvent.RemoveListener(listener);
+		}
+	}
+	
+	public static void TriggerEvent(string eventName) {
+		UnityEvent thisEvent = null;
+		if (instance.dictionary.TryGetValue(eventName, out thisEvent)) {
+			thisEvent.Invoke();
+		}
+	}
+		
+}
---
+++ Assets/scripts/TestEvent.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestEvent : MonoBehaviour {
+
+	// Use this for initialization
+	void OnEnable () {
+		EventManager.StartListening("Timer", TimeFunction);
+	}
+	
+	void OnDisable() {
+		EventManager.StopListening("Timer", TimeFunction);
+	}
+	
+	// Update is called once per frame
+	private float dt = 0;
+	void Update () {
+		dt += Time.deltaTime;
+		if (dt > 5.0f) {
+			EventManager.TriggerEvent("Timer");
+			dt = 0.0f;
+		}
+	}
+	
+	void TimeFunction() {
+		TestModalPanel tmp = FindObjectOfType(typeof (TestModalPanel)) as TestModalPanel;
+		tmp.testPanel();
+	}
+}
