b32057e033c9d36d159e9a189c352249b6f55d85
Joe Pelz <joe.pelz@gmail.com>
2016-05-16 16:04:14 -0700

    starting food selection

---
+++ Assets/scripts/fridge.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class fridge : MonoBehaviour {
+
+    public GameObject circleFat;
+    public GameObject circleVeggy;
+    public GameObject circleMeat;
+
+    // Use this for initialization
+    void Start () {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetMouseButtonDown(0)) {
+            var worldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            var mousePosition = new Vector2(worldPos.x, worldPos.y);
+            var hitCollider = Physics2D.OverlapPoint(mousePosition);
+
+            if (hitCollider) {
+                circleFat.transform.position = new Vector3(
+                    hitCollider.transform.position.x,
+                    hitCollider.transform.position.y,
+                    0);
+                Debug.Log("Hit " + hitCollider.transform.name + " x" + hitCollider.transform.position.x + " y " + hitCollider.transform.position.y);
+            }
+        }
+    }
+}
