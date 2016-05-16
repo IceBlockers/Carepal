4a5da09ee938b6568ad8d749efecce6007ec118d
Joe Pelz <joe.pelz@gmail.com>
2016-05-16 16:39:23 -0700

    fridge selecting works.

--- Assets/scripts/fridge.cs
+++ Assets/scripts/fridge.cs
@@ -6,6 +6,11 @@ public class fridge : MonoBehaviour {
     public GameObject circleFat;
     public GameObject circleVeggy;
     public GameObject circleMeat;
+    public GameObject btnAdvance;
+
+    private bool bChoseFat;
+    private bool bChoseVeggy;
+    private bool bChoseMeat;
 
     // Use this for initialization
     void Start () {
@@ -18,14 +23,43 @@ public class fridge : MonoBehaviour {
             var worldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             var mousePosition = new Vector2(worldPos.x, worldPos.y);
             var hitCollider = Physics2D.OverlapPoint(mousePosition);
-
+            
             if (hitCollider) {
-                circleFat.transform.position = new Vector3(
-                    hitCollider.transform.position.x,
-                    hitCollider.transform.position.y,
-                    0);
-                Debug.Log("Hit " + hitCollider.transform.name + " x" + hitCollider.transform.position.x + " y " + hitCollider.transform.position.y);
+                if (hitCollider.tag.Contains("fat")) {
+                    placeCircle(hitCollider, circleFat);
+                    bChoseFat = true;
+                } else if (hitCollider.tag.Contains("meat")) {
+                    placeCircle(hitCollider, circleMeat);
+                    bChoseMeat = true;
+                } else if (hitCollider.tag.Contains("veggy")) {
+                    placeCircle(hitCollider, circleVeggy);
+                    bChoseVeggy = true;
+                } else {
+                    //hit the advance button.
+                    Debug.Log("Moving on to the next scene!");
+                }
+                updateButton();
             }
+        }
+    }
+
+    void placeCircle(Collider2D hitCollider, GameObject circle) {
+        circle.transform.position = new Vector3(
+            hitCollider.transform.position.x,
+            hitCollider.transform.position.y,
+            0);
+    }
+
+    void updateButton() {
+        if (bChoseFat && bChoseMeat && bChoseVeggy) {
+            Vector3 pos = btnAdvance.transform.position;
+            pos.x = 6.621002f;
+            pos.y = -3.56601f;
+            btnAdvance.transform.position = pos;
+        } else {
+            Vector3 pos = btnAdvance.transform.position;
+            pos.y = -1000;
+            btnAdvance.transform.position = pos;
         }
     }
 }
