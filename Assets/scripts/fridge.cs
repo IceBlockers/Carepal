4292f07379c4f7119be3ce9faedb07b140160b00
Joe Pelz <joe.pelz@gmail.com>
2016-05-16 17:05:30 -0700

    selection is saved. Ready to load next level

--- Assets/scripts/fridge.cs
+++ Assets/scripts/fridge.cs
@@ -12,6 +12,10 @@ public class fridge : MonoBehaviour {
     private bool bChoseVeggy;
     private bool bChoseMeat;
 
+    private string fatSprite;
+    private string veggySprite;
+    private string meatSprite;
+
     // Use this for initialization
     void Start () {
 
@@ -25,22 +29,51 @@ public class fridge : MonoBehaviour {
             var hitCollider = Physics2D.OverlapPoint(mousePosition);
             
             if (hitCollider) {
+                var renderer = hitCollider.GetComponent<SpriteRenderer>();
                 if (hitCollider.tag.Contains("fat")) {
                     placeCircle(hitCollider, circleFat);
                     bChoseFat = true;
+                    fatSprite = renderer.sprite.name;
                 } else if (hitCollider.tag.Contains("meat")) {
                     placeCircle(hitCollider, circleMeat);
                     bChoseMeat = true;
+                    meatSprite = renderer.sprite.name;
                 } else if (hitCollider.tag.Contains("veggy")) {
                     placeCircle(hitCollider, circleVeggy);
                     bChoseVeggy = true;
+                    veggySprite = renderer.sprite.name;
                 } else {
-                    //hit the advance button.
-                    Debug.Log("Moving on to the next scene!");
+                    //you clicked the arrow button.
+                    //Debug.Log("Moving on to the next scene!");
+                    saveChoices();
+                    
+                    //TODO:  Load new level here
+
                 }
                 updateButton();
             }
         }
+    }
+
+    void saveChoices() {
+        if (fatSprite.Contains("cheese")) { PlayerPrefs.SetInt("food1", 0); }
+        else if (fatSprite.Contains("avocad")) { PlayerPrefs.SetInt("food1", 1); }
+        else if (fatSprite.Contains("butter")) { PlayerPrefs.SetInt("food1", 2); }
+
+        if (veggySprite.Contains("lettu")) { PlayerPrefs.SetInt("food2", 3); }
+        else if (veggySprite.Contains("onion")) { PlayerPrefs.SetInt("food2", 4); }
+        else if (veggySprite.Contains("tomat")) { PlayerPrefs.SetInt("food2", 5); }
+
+        if (meatSprite.Contains("patty")) { PlayerPrefs.SetInt("food3", 6); }
+        else if (meatSprite.Contains("roast")) { PlayerPrefs.SetInt("food3", 7); }
+        else if (meatSprite.Contains("sandw")) { PlayerPrefs.SetInt("food3", 8); }
+
+        /*
+        Debug.Log(string.Format("Saved: {0}, {1}, {2}",
+            PlayerPrefs.GetInt("food1"),
+            PlayerPrefs.GetInt("food2"),
+            PlayerPrefs.GetInt("food3")));
+        */
     }
 
     void placeCircle(Collider2D hitCollider, GameObject circle) {
