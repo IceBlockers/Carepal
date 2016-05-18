9d10dac06282af14da5ad7e9dd40b47dcd31ec1f
Joe Pelz <joe.pelz@gmail.com>
2016-05-18 14:20:27 -0700

    Works on android again. Whew!

---
+++ Assets/scripts/Textdbg.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Textdbg : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetButton("Fire1")) {
+            Text text = gameObject.GetComponent<Text>();
+            Vector3 mouse = Input.mousePosition;
+            Vector2 pos = new Vector2(mouse.x, mouse.y);
+            Vector3 wmouse = Camera.main.ScreenToWorldPoint(mouse);
+            Vector2 wpos = new Vector2(wmouse.x, wmouse.y);
+
+            PlayerPrefs.SetFloat("Timer", Time.deltaTime);
+
+            text.text = "Fire! " + pos;
+            text.text += "\nWorld: " + wpos;
+            text.text += "\nTutorial: " + PlayerPrefs.GetInt("Tutorial");
+            text.text += "\nHunger: " + PlayerPrefs.GetInt("Hunger");
+            text.text += "\nTime: " + PlayerPrefs.GetFloat("Timer");
+        }
+	}
+}
